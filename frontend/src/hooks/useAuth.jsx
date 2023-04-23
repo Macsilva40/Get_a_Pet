6449cec4a6f6a3bc2ecb02ca868b99103591e471
Macsilva40 <mdprints@hotmail.com>
2023-04-23 14:25:51 -0400

    Pagina de Login

---
+++ frontend/src/hooks/useAuth.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import api from '../utils/api'
+
+
+// import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export default function useAuth() {
+    async function register(user) {
+
+        try {
+            const data = await api.post('/users/register', user).then((response) => {
+                return response.data
+            })
+
+            console.log(data)
+        } catch (error) {
+            // tratar o erro
+            console.log(error)
+        }
+
+    }
+
+    return { register }
+}
