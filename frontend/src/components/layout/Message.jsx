2e23fa872d9fe6a8bfb6290864822509da628fb9
Macsilva40 <mdprints@hotmail.com>
2023-04-24 22:00:33 -0400

    função loagado

--- frontend/src/components/layout/Message.jsx
+++ frontend/src/components/layout/Message.jsx
@@ -4,14 +4,15 @@ import bus from "../../utils/bus";
 import styles from "./Message.module.css";
 
 function Message() {
-    let [visibility, setVisibility] = useState(false);
-    let [message, setMessage] = useState("");
-    let [type, setType] = useState("");
+    const [visibility, setVisibility] = useState(false);
+    const [message, setMessage] = useState("");
+    const [type, setType] = useState("");
 
     useEffect(() => {
         bus.addListener("flash", ({ message, type }) => {
             setVisibility(true);
             setMessage(message);
+
             setType(type);
             setTimeout(() => {
                 setVisibility(false);
