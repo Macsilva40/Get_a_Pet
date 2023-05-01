4026be2eb2c8b193e56e6a872f5e6ddb82c8fc63
Macsilva40 <mdprints@hotmail.com>
2023-05-01 19:28:40 -0400

    Formulario de Pet

--- frontend/src/components/form/PetForm.jsx
+++ frontend/src/components/form/PetForm.jsx
@@ -1,24 +1,56 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 import formStyles from './Form.module.css'
 
-import Input from "./Input"
-import Select from "./Select"
+import Input from './Input'
+import Select from './Select'
 
-function PetForm(handleSubmit, petData, btnText) {
+function PetForm({ handleSubmit, petData, btnText }) {
     const [pet, setPet] = useState(petData || {})
     const [preview, setPreview] = useState([])
-    const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo', 'Mesclado']
+    const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo']
 
-    function onFileChange(e) { }
+    function onFileChange(e) {
+        setPreview(Array.from(e.target.files))
+        setPet({ ...pet, images: [...e.target.files] })
+    }
 
-    function handleChange(e) { }
+    function handleChange(e) {
+        setPet({ ...pet, [e.target.name]: e.target.value })
+    }
 
-    function handleColor(e) { }
+    function handleColor(e) {
+        setPet({
+            ...pet,
+            color: e.target.options[e.target.selectedIndex].text
+        })
+    }
 
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(pet)
+    }
 
     return (
-        <form className={formStyles.form_container}>
+        <form onSubmit={submit} className={formStyles.form_container}>
+            <div className={formStyles.preview_pet_images}>
+                {preview.length > 0
+                    ? preview.map((image, index) => (
+                        <img
+                            src={URL.createObjectURL(image)}
+                            alt={pet.name}
+                            key={`${pet.name}+${index}`}
+                        />
+                    ))
+                    : pet.images &&
+                    pet.images.map((image, index) => (
+                        <img
+                            src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                            alt={pet.name}
+                            key={`${pet.name}+${index}`}
+                        />
+                    ))}
+            </div>
             <Input
                 text="Imagens do Pet"
                 type="file"
